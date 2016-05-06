8ccb12989f7cafd71704c627e1860d0fca663d29
luckyworld <villat.guillaume@gmail.com>
2016-05-06 15:44:16 +0200

    Shop + image + element graphique

--- js/app.js
+++ js/app.js
@@ -1,54 +1,15 @@
-// Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
-  $('.button-collapse').sideNav({
-      menuWidth: 200, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  $('.button-collapse').sideNav('show');
+        $(document).ready(function(){
+          $('.button-collapse').sideNav({
+            menuWidth: 900, // Default is 240
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            }
+          );
 
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-  });
-
-
-  $(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
-        
-
-
-  var options = [
-    {selector: '.class', offset: 200, callback: customCallbackFunc } },
-    {selector: '.other-class', offset: 200, callback: function() {
-      customCallbackFunc();
-    } },
-  ];
-  Materialize.scrollFire(options);
-
-
-
-  $(document).ready(function(){
-    $('.collapsible').collapsible({
-      expandable : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-  });
-
-    $(document).ready(function(){
-      $('.carousel').carousel();
-    });
-        
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-        
-// Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', [3]); // Move next n times.
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', [4]); // Move prev n times.
-      
+          $('.scrollspy').scrollSpy();
+          $('.slider').slider({
+            indicators: false,
+            interval: 1000
+          });
+          $('.materialboxed').materialbox();
+        });
